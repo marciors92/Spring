45e93f6569a1b84d52b78cfd209c2dd21c301d25
unknown <marciors92@gmail.com>
2021-10-02 15:05:20 -0300

    Loja de Games

---
+++ lojadegames/src/main/java/br/org/generation/lojadegames/model/Produto.java
@@ -0,0 +1,85 @@
+package br.org.generation.lojadegames.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 3, max = 100)
+	private String nome;
+	
+	@NotNull
+	@Min(0)
+	private int qtdeEstoque;
+	
+	@NotNull
+	@Positive
+	@Digits(integer = 3, fraction = 2, message = "o valor do jogo só pode ser em centenas e duas casas após o ponto")
+	private BigDecimal valor;
+	
+	@ManyToOne
+	@JsonIgnoreProperties ("produto")
+	private Categoria categoria;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getQtdeEstoque() {
+		return qtdeEstoque;
+	}
+
+	public void setQtdeEstoque(int qtdeEstoque) {
+		this.qtdeEstoque = qtdeEstoque;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+
+}
